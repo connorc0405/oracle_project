6d2b806d307342db9970822310315eab0db81a0e
ryandean <rdean@US-504-119.amsacs.org>
2015-05-07 13:33:25 -0400

    Added glyphicons.  Added scrolling effect.  Progressed with the menu design.

--- Ryan_Live_Project/js/script.js
+++ Ryan_Live_Project/js/script.js
@@ -22,7 +22,25 @@ databaseApp.config(function($routeProvider) {
 });
 
 databaseApp.controller('welcome-ctrl', function($scope){
-
+	// Code to make the page scroll to a specific section.  Code idea from http://jsfiddle.net/ryXFt/3/
+	$('.student-link').click(function(){
+		$('html,body').animate({
+			scrollTop:
+			$('#students').offset().top-50},
+				'slow');
+		});
+	$('.parent-link').click(function(){
+		$('html,body').animate({
+			scrollTop:
+			$('#parents').offset().top-50},
+				'slow');
+		});
+	$('.admin-link').click(function(){
+		$('html,body').animate({
+			scrollTop:
+			$('#admin').offset().top-50},
+				'slow');
+		});
 });
 
 databaseApp.controller('construction-ctrl', function($scope){
