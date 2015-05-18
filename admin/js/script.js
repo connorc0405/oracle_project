b610ebcc0d8760439320faacdfa98789d59dbb18
ryandean <rdean@US-504-119.amsacs.org>
2015-05-18 14:06:23 -0400

    Continued styling Devon's code into the project under the admin section.

--- admin/js/script.js
+++ admin/js/script.js
@@ -16,8 +16,15 @@ adminApp.config(function($routeProvider){
 		controller: 'new-course-ctrl'
 	})
 
+	.when('/course-profile',
+	{
+		templateUrl: 'php/course-profile.php',
+		controller: 'course-profile-ctrl'
+	})
+
 	.otherwise({redirectTo: '/'});
 });
 
 adminApp.controller('admin-home-ctrl', function($scope){});
 adminApp.controller('new-course-ctrl', function($scope){});
+adminApp.controller('course-profile-ctrl', function($scope){});
