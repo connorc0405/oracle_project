8e4d7d03793f410093e30b22b6803b7c2e7b4781
ryandean <rdean@US-504-119.amsacs.org>
2015-05-20 12:54:17 -0400

    Got new courses working with the database.  Furhter styling.

--- admin/php/course-profile.php
+++ admin/php/course-profile.php
@@ -1,17 +1,13 @@
-
-
-			<?php
-				$link = mysqli_connect("localhost","root","","StudentDatabase");
-
-				$sql = "Select * from courses where course_id=(
-					Select max(course_id) From courses)";
-				$result = mysqli_query($link, $sql);
-				$row = mysqli_fetch_assoc($result);
-				$id=(int)$row["course_id"]+1;     
-
-				$adds['class_name'] = $link->real_escape_string($_POST['class_name']);
-				$adds['teacher'] = $link->real_escape_string($_POST['teacher']);
-				$adds['room_num'] = $link->real_escape_string($_POST['room_num']);
+<?php
+	$link = mysqli_connect("localhost","root","","StudentDatabase");
+	$sql = "Select * from courses where course_id=(
+			Select max(course_id) From courses)";
+	$result = mysqli_query($link, $sql);
+	$row = mysqli_fetch_assoc($result);
+	$id=(int)$row["course_id"]+1;	  
+	$adds['class_name'] = $link->real_escape_string($_POST['class_name']);
+	$adds['teacher'] = $link->real_escape_string($_POST['teacher']);
+	$adds['room_num'] = $link->real_escape_string($_POST['room_num']);
 				$adds['period'] = $link->real_escape_string($_POST['period']);
 
 				mysqli_query($link,"INSERT INTO courses (`course_id`, `class_name`, 
@@ -19,18 +15,24 @@
 				VALUES ($id, '". $adds['class_name']. "', '". $adds['teacher']. "', 
 					'". $adds['room_num']. "', '". $adds['period']. "')") 
 				or die(mysqli_error($link));
-
-
 			?>
-
-		<center>
-			<img src="AMSA_logo.jpg" style="width:500px;height:225px">
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Course Created</title>
+	
+	<link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Class Created Successfully</h1>
+			</div>
+			
 			<table>
-				<tr>
-					<td>
-						<b>Class Created Successfully</b>
-					</td>
+						
 				</tr>
 				<tr>
 					<td>
@@ -61,10 +63,16 @@
 
 			<br><br>
 			<form action="CourseLogInfoWebpage.php" method="post" enctype="multipart/form-data">
-			<input id="submit" type="submit" value="Enter Another Class">
+			<input class="btn btn-default"id="submit" type="submit" value="Enter Another Class">
 			</form>
 
 			<br><br><br><br>
 			<form action="Homepage.html" method="post" enctype="multipart/form-data">
 			<input id="submit" type="submit" value="Home">
 			</form>
+		</div>
+	</div>
+
+</body>
+</html>
+		
--- admin/php/new-course.php
+++ admin/php/new-course.php
@@ -9,7 +9,7 @@
 		<div class="col-xs-6 panel panel-default">
 			<div class="panel-body">
 			<h3>New Course</h3>
-				<form action="#course-profile" method="post" enctype="multipart/form-data">
+				<form action="php/course-profile.php" method="post" enctype="multipart/form-data">
 					<div class="form-group">
 						<label for="class_name" class="form-label">Course Name</label>
 						<input type="text" class="form-control" name="class_name" id="class_name" placeholder="Enter Course Name">
@@ -37,23 +37,18 @@
 		</div>
 	</div>
 
-	<div class="row">
+	<div class="row" id="new-course-courses"> 
 		<div class="col-xs-3"></div>
 		<div class="col-xs-6 panel panel-default">
-			<div class="panel-body">
+			<div class="panel-body" >
 				<h3>Available Courses</h3>
-				
-				<style type="text/css">
-					tr.top td { border-top: thin solid black; }
-					tr.bottom td { border-bottom: thin solid black; }
-					tr.row td:first-child { border-left: thin solid black; }
-					tr.row td:last-child { border-right: thin solid black; }
-				</style>
-
-				<table>
-					<tr class="top bottom row">
-						<!-- <td style="width:140px">Course Id</td> -->
-						<td style="width:140px">Class Name</td>
+				<table class="table" id="new-course-tbl">
+					<tr>
+						<th>ID</th>
+						<th>Course Name</th>
+						<th>Teacher Name</th>
+						<th>Room #</th>
+						<th>Period</th>
 						<!-- <td style="width:140px">Teacher</td>
 						<td style="width:140px">Room Number</td>
 						<td style="width:140px">Period</td> -->
@@ -75,7 +70,13 @@
 						$teacher=$row["teacher"];
 						$room_num=$row["room_num"];
 						$period=$row["period"];
-						echo "<tr><td>$class_name</td></tr>";
+						echo "<tr>
+								<td>$course_id</td>
+								<td>$class_name</td>
+								<td>$teacher</td>
+								<td>$room_num</td>
+								<td>$period</td>
+							</tr>";
 						$count=$count-1;
 					}
 
