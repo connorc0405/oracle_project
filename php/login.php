e6f3cda1bf9e214445fbf7209b32721ab73dc6e9
ryandean98 <ryandean98@bitbucket.org>
2015-06-16 11:28:33 -0400

    Started code organization.  Added a no user found error page.

--- php/login.php
+++ php/login.php
@@ -1,31 +1,35 @@
-<html>
 <?php
-$input_username = $_POST['username'];
-$input_password = $_POST['password'];
-$cstrong;
-$connection = mysqli_connect("localhost","root","","studentdatabase");
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-$hash_salt_object = mysqli_query($connection, "SELECT hash, salt FROM login_information WHERE username = '".$input_username."';") or die("Error description: ".mysqli_error    ($connection));
-$row = mysqli_fetch_assoc($hash_salt_object);
-$hash = $row['hash'];
-$salt = $row['salt'];
-//echo "hash: ".$hash."      salt: ".$salt;
-if(hash('sha512', $input_password.$salt, false) === $hash)
-{
-	echo "Great Success";
-	
-	header('Location: ../#construction');
-  	exit;
-}
-else{
-	echo "Failed login";
-}
-//echo "input hash:  " . (hash('sha512', $input_password.$salt, false));
-//echo "input password + salt:   ".$input_password.$salt;
-mysqli_close($connection);
-?>
+	include_once('../admin/php/Database.php');
+	/* Gets the form input from html/login.html */
+	$input_username = $_POST['username'];
+	$input_password = $_POST['password'];
+	$cstrong;
 
-</html>
+	/* Sets the connection to the mySQL database, and tests the connection throwing an error if needed */
+	$connection = mysqli_connect("localhost","root","","studentdatabase");
+	
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+	$hash_salt_object = mysqli_query($connection, "SELECT hash, salt FROM login_information WHERE username = '".$input_username."';") or die("Error description: ".mysqli_error($connection));
+	$row = mysqli_fetch_assoc($hash_salt_object);
+	$hash = $row['hash'];
+	$salt = $row['salt'];
+	//echo "hash: ".$hash."      salt: ".$salt;
+	if(hash('sha512', $input_password.$salt, false) === $hash)
+	{
+		echo "Great Success";
+		
+		header('Location: ../#construction');
+	  	exit;
+	}
+	else{
+		echo "Failed login";
+		header('Location: ../html/404.html');
+	  	exit;
+	}
+	//echo "input hash:  " . (hash('sha512', $input_password.$salt, false));
+	//echo "input password + salt:   ".$input_password.$salt;
+	mysqli_close($connection);
+?>
--- php/register.php
+++ php/register.php
@@ -1,39 +1,50 @@
-<html>
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-$cstrong;
-$salt = openssl_random_pseudo_bytes(10, $cstrong);
-$connection = mysqli_connect("localhost","root","","StudentDatabase");
+  /*
+   * Gets the username and password from the form on html/register.html 
+  */
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $cstrong;
+  $salt = openssl_random_pseudo_bytes(10, $cstrong);
+  $connection = mysqli_connect("localhost","root","","StudentDatabase");
 
+  /*
+   * Gets the last entry in the login_information table
+  */
+  $sql = "SELECT *
+          FROM login_information
+          WHERE id=(
+                    SELECT MAX(id)
+                    FROM login_information)";
 
-$sql = "Select * from login_information where id=(
-          Select max(id) From login_information)";
-$result = mysqli_query($connection, $sql);
-$row = mysqli_fetch_assoc($result);
+  $result = mysqli_query($connection, $sql);
+  $row = mysqli_fetch_assoc($result);
 
+  /* Increments a number to set as the Unique ID (UID) */
+  $id = (int)$row["id"]+1;
 
-$id = (int)$row["id"]+1;
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} 
-else {
-	echo "Connected to MySQL Server" . PHP_EOL;
-}
-if (!mysqli_query($connection, "CREATE TABLE IF NOT EXISTS login_information (id INT(4) PRIMARY KEY, username VARCHAR(45), hash VARCHAR(128), salt VARCHAR(10))")) {
-  echo "Error description: " . mysqli_error($connection);
+  /* If error connecting to database, throw error */
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  } 
+  else {
+  	echo "Connected to MySQL Server" . PHP_EOL;
   }
-if (!mysqli_query($connection, "INSERT INTO login_information (id, username, hash, salt) VALUES ('".$id."', '" .$username."', sha2('".$password."".$salt."', 512), '".$salt."');")) {
-  echo "Error description: " . mysqli_error($connection);
-  }
-else{
-	echo "Account created successfully";
 
-  // Database::setPendingRow($id, $username, $hash, $salt);
-	header('Location: ../admin/#new-student');
-  	exit;
-}
-mysqli_close($connection);
+  /* If the table does not exist in the studentdatabase, create a login_information table for
+      everyone's account information */
+  if (!mysqli_query($connection, "CREATE TABLE IF NOT EXISTS login_information (id INT(4) PRIMARY KEY, username VARCHAR(45), hash VARCHAR(128), salt VARCHAR(10))")) {
+    echo "Error description: " . mysqli_error($connection);
+  }
+
+  /* If there is an error throw one, else insert the row into the login_information table */
+  if (!mysqli_query($connection, "INSERT INTO login_information (id, username, hash, salt) VALUES ('".$id."', '" .$username."', sha2('".$password."".$salt."', 512), '".$salt."');")) {
+    echo "Error description: " . mysqli_error($connection);
+  }
+  else {
+  	echo "Account created successfully";
+  	header('Location: ../admin/#new-student');
+    exit;
+  }
+  mysqli_close($connection);
 ?>
-
-</html>
