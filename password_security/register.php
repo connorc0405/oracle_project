8c07d0858d582352fedde378320a1761ef810b06
connorc0405 <connorc0405@gmail.com>
2016-04-06 11:21:02 -0400

    Update register.php

--- password_security/register.php
+++ password_security/register.php
@@ -2,7 +2,6 @@
 <?php
 $username = $_POST['username'];
 $password = $_POST['password'];
-$secret = $_POST['secret'];
 $cstrong;
 $salt = openssl_random_pseudo_bytes(10, $cstrong);
 $connection = mysqli_connect("192.168.1.177","root","ccumming","sqlinjection");
@@ -13,11 +12,11 @@ if (mysqli_connect_errno())
 else{
 	echo "Connected to MySQL Server" . PHP_EOL;
 }
-if (!mysqli_query($connection, "CREATE TABLE IF NOT EXISTS login_information (username VARCHAR(45), secret VARCHAR (45), hash VARCHAR(128), salt VARCHAR(10))"))
+if (!mysqli_query($connection, "CREATE TABLE IF NOT EXISTS login_information (username VARCHAR(45), hash VARCHAR(128), salt VARCHAR(10))"))
   {
   echo "Error description: " . mysqli_error($connection);
   }
-if (!mysqli_query($connection, "INSERT INTO login_information (username, secret, hash, salt) VALUES ('".$username."', '".$secret."', sha2('".$password."".$salt."', 512), '".$salt."');"))
+if (!mysqli_query($connection, "INSERT INTO login_information (username, hash, salt) VALUES ('".$username."', sha2('".$password."".$salt."', 512), '".$salt."');"))
   {
   echo "Error description: " . mysqli_error($connection);
   }
