d273160b25d6ae2e110274c542fed1e5795ef891
connorc0405 <connorc0405@gmail.com>
2016-04-07 11:09:45 -0400

    Create login_with_secret_nohash.php

---
+++ password_security/login_with_secret_nohash.php
@@ -0,0 +1,20 @@
+<html>
+<?php
+//Get input username and password, set up connection
+$input_username = $_POST['username'];
+$input_password = $_POST['password'];
+$cstrong;
+$connection = mysqli_connect("10.0.4.206","root","ccumming","sqlinjection");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+$secret_object = mysqli_query($connection, "SELECT secret FROM login_information WHERE username = '".$input_username."' or die(mysql_error($connection));
+$row = mysqli_fetch_assoc($secret_object);
+$secret = $row['secret'];
+mysqli_close($connection);
+echo $input_username."'s secret is: ".$secret;
+?>
+
+</html>
