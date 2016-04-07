9473842ad6f1b6e22e78b7e143c24dd070c59bad
connorc0405 <connorc0405@gmail.com>
2016-04-07 14:08:56 -0400

    Update login_with_secret_nohash.php

--- password_security/login_with_secret_nohash.php
+++ password_security/login_with_secret_nohash.php
@@ -10,7 +10,7 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
   
-$secret_object = mysqli_query($connection, "SELECT secret FROM login_information WHERE username = '".$input_username."' AND password = '".$input_password."' or die(mysql_error($connection)");
+$secret_object = mysqli_query($connection, "SELECT secret FROM login_information WHERE username = '".$input_username."' AND password = '".$input_password."';") or die(mysql_error($connection));
 $row = mysqli_fetch_assoc($secret_object);
 $secret = $row['secret'];
 mysqli_close($connection);
