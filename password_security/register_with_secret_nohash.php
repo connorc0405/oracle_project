c695e75746c4fd99d39b8369d4baee825a27c1ba
connorc0405 <connorc0405@gmail.com>
2016-04-07 11:07:29 -0400

    Create register_with_secret_nohash.php

---
+++ password_security/register_with_secret_nohash.php
@@ -0,0 +1,30 @@
+<html>
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$secret = $_POST['secret'];
+$cstrong;
+$salt = openssl_random_pseudo_bytes(10, $cstrong);
+$connection = mysqli_connect("10.0.4.206","root","ccumming","sqlinjection");
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else{
+	echo "Connected to MySQL Server" . PHP_EOL;
+}
+if (!mysqli_query($connection, "CREATE TABLE IF NOT EXISTS login_information (username VARCHAR(45), secret VARCHAR (45),)"))
+  {
+  echo "Error description: " . mysqli_error($connection);
+  }
+if (!mysqli_query($connection, "INSERT INTO login_information (username, secret) VALUES ('".$username."', '".$secret."');"))
+  {
+  echo "Error description: " . mysqli_error($connection);
+  }
+else{
+	echo "Account created successfully";
+}
+mysqli_close($connection);
+?>
+
+</html>
