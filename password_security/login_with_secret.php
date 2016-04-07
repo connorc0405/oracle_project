f3bc29d64fd1d08dd7caf03c6657665801754b57
connorc0405 <connorc0405@gmail.com>
2016-04-07 10:29:53 -0400

    Update login_with_secret.php

--- password_security/login_with_secret.php
+++ password_security/login_with_secret.php
@@ -12,13 +12,13 @@ if (mysqli_connect_errno())
   }
 
 //Get hash and salt using the given username.
-$hash_salt_object = mysqli_query($connection, "SELECT hash, salt FROM login_information WHERE username = '".$input_username."';") or die("Error description: ".mysqli_error    ($connection));
+$hash_salt_object = mysqli_query($connection, "SELECT hash, salt FROM login_information WHERE username = '$input_username';") or die("Error description: ".mysqli_error    ($connection));
 $row = mysqli_fetch_assoc($hash_salt_object);
 $hash = $row['hash'];
 $salt = $row['salt'];
 
 //Select secret by checking hash of given password + salt against the saved salt variable from the previous statement
-$secret_object = mysqli_query($connection, "SELECT secret FROM login_information WHERE username = '".$input_username."' AND hash = SHA2('".$input_password.$salt."', 512);") or die(mysql_error($connection));
+$secret_object = mysqli_query($connection, "SELECT secret FROM login_information WHERE username = '$input_username' AND hash = SHA2('$input_password.$salt', 512);") or die(mysql_error($connection));
 $row2 = mysqli_fetch_assoc($secret_object);
 $secret = $row2['secret'];
 
